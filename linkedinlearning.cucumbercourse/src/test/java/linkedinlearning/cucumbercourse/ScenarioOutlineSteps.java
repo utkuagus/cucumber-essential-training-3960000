2806eff239fecb3c61e50b34445d0fd8329fb8ad
cucumbercourse <coursecucumber@gmail.com>
2025-06-27 22:14:14 -0700

    Chapter 7 Artifacts

--- linkedinlearning.cucumbercourse/src/test/java/linkedinlearning/cucumbercourse/ScenarioOutlineSteps.java
+++ linkedinlearning.cucumbercourse/src/test/java/linkedinlearning/cucumbercourse/ScenarioOutlineSteps.java
@@ -10,6 +10,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import com.microsoft.playwright.BrowserType.LaunchOptions;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+
 public class ScenarioOutlineSteps {
 
   int InitialBillAmount;
@@ -38,24 +43,31 @@ public class ScenarioOutlineSteps {
     System.out.println("Expected Value: " + expectedValue);
     System.out.println("Calculated Value: " + SystemCalcValue);
     assertThat(expectedValue).isEqualTo(SystemCalcValue);
-    //web automation code
-    String WebpageResponse = invokeWebPage(expectedValue);
-    //safer option is to use WebpageResponse.endsWith
-    assertThat(WebpageResponse.endsWith("Final Bill Amount is: $" + expectedValue)).isTrue();
+
+    // ********Begin Code for Selenium based Automation*********/
+    // String WebpageResponse = invokeWebPage(expectedValue);
+    // safer option is to use WebpageResponse.endsWith
+    // assertThat(WebpageResponse.endsWith("Final Bill Amount is: $" +
+    // expectedValue)).isTrue();
+    // ********End Code for Selenium based Automation*********/
+
+    String WebpageResponse = invokeWebPagePlaywright(expectedValue);
+    System.out.println("Page Response: " + WebpageResponse);
+    assertThat(WebpageResponse.contains("Final Bill Amount is: $" + expectedValue)).isTrue();
   }
 
   private String invokeWebPage(double expectedValueForWebpage) {
     // set chrome options to not use sandbox
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--no-sandbox");
-    //initialize web driver
+    // initialize web driver
     WebDriver driver = new ChromeDriver(options);
     driver.get("http://127.0.0.1:8000");
-    //set the initial bill amount value on web page's bill amount textbox
+    // set the initial bill amount value on web page's bill amount textbox
     WebElement BillAmountTextBox = driver.findElement(By.id("id_billamount"));
     CharSequence InitialBillAmountString = Integer.toString(InitialBillAmount);
     BillAmountTextBox.sendKeys(InitialBillAmountString);
-    //set tax rate on web page's tax rate
+    // set tax rate on web page's tax rate
     WebElement TaxRateTextBox = driver.findElement(By.id("id_taxrate"));
     CharSequence TaxRateString = Double.toString(TaxRate);
     TaxRateTextBox.sendKeys(TaxRateString);
@@ -66,10 +78,36 @@ public class ScenarioOutlineSteps {
     WebElement Header3Element = driver.findElement(By.xpath("//h3"));
     System.out.println(Header3Element.getText());
     String WebPageResponse = Header3Element.getText();
-    //close page
+    // close page
     driver.quit();
-    //return response
+    // return response
     return WebPageResponse;
   }
 
+  private String invokeWebPagePlaywright(double expectedValueForWebpage) {
+    
+    System.out.println("CALLING PLAYWRIGHT");
+    String Response = "";
+    try (Playwright playwright = Playwright.create()) {
+      //launch browser with the right settings
+      LaunchOptions options = new LaunchOptions();
+      options.headless = false;
+      options.chromiumSandbox = false;
+      options.setSlowMo(1000);
+      Browser browser = playwright.chromium().launch(options);
+      Page page = browser.newPage();
+      //navigate to the Python Djanfo website
+      page.navigate("http://127.0.0.1:8000");
+      //set values
+      page.fill("#id_billamount", Integer.toString(InitialBillAmount));
+      page.fill("#id_taxrate", Double.toString(TaxRate));
+      //click button
+      page.waitForSelector("#mybutton");
+      page.click("#mybutton");
+      //read and return response
+      Response = page.textContent("h3:first-of-type");
+      browser.close();
+      return Response;
+    }
+  }
 }
